906941a047764cdca93d11bae4d5fa44fa719dbe
Eugenio Thieme <eugen@thieme.it>
2016-11-30 10:33:00 +0100

    Implementato controllo eof
    Il programma legge correttamente i parametri da file, li salva in vettori, chiama la funzione e
    salva i risultati su file.

--- displacement.f90
+++ displacement.f90
@@ -1,6 +1,46 @@
-import 'okada.f'
-program displacement
-!  implicit none
-!  include 'okada.f90'
+program prova
+  implicit none
+  real(kind=4), dimension(5) :: alpha, x, y, z, depth, dip, pot1, pot2, pot3, pot4 ! input values for dc3d0
+  real(kind=4), dimension(5) :: ux, uy, uz, uxx, uyx, uzx, uxy, uyy, uzy, uxz, uyz, uzz, iret ! values returned by dc3d0
+  integer :: out_unit = 1, in_unit = 2 ! opening i/o devices, please don't use 5 or 6 (special meaning)
+  integer :: stat ! this is for checking eof while reading file
+  integer :: j=1, i ! those are counters
 
-end program displacement
+  open(unit=in_unit, file='param.txt', action='read') 
+  
+  !Reading file
+  do 
+    read(in_unit, *, iostat=stat) alpha(j), x(j), y(j), z(j), depth(j), dip(j), pot1(j), pot2(j), pot3(j), pot4(j)
+    !if(stat /= 0) exit   checking if eof
+    if(stat < 0) then
+      print *, 'Reached eof'
+      j = j-1
+      exit
+    else if(stat > 0) then
+      print *, 'Error in file format'
+      exit
+    end if
+    j = j+1
+  end do
+   
+  
+  !Calling subroutine and storing results in the i-th component of an array
+  do i=1, j
+    call dc3d0(alpha(i), x(i), y(i), z(i), depth(i), dip(i), pot1(i), pot2(i), pot3(i),&
+               & pot4(i), ux(i), uy(i), uz(i), uxx(i), uyx(i), uzx(i), uxy(i), uyy(i) &
+               &, uzy(i), uxz(i), uyz(i), uzz(i), iret(i))
+  end do
+ 
+
+  open(unit=out_unit, file='results.txt', action='write', status='replace')
+ 
+  !Writing results on file 
+  do i = 1, j
+    write(out_unit, *) x(i), y(i), uz(i) 
+
+  end do  
+
+  close(out_unit)
+  close(in_unit)
+ 
+end program prova
--- prova.f90
+++ prova.f90
@@ -1,28 +1,45 @@
 program prova
   implicit none
-  real :: alpha, x, y, z, depth, dip, pot1, pot2, pot3, pot4 
-  real(kind=4) :: ux, uy, uz, uxx, uyx, uzx, uxy, uyy, uzy, uxz, uyz, uzz, iret
-  integer :: out_unit, in_unit
-
- ! print *,'pass medium constant'
- ! read *, alpha
- !print *, 'pass coordinates of observation points'
- ! read *, x, y, z
- ! print *, 'depth of source point:'
- ! read *, depth
- ! print *, 'dip angle:'
- ! read *, dip
- ! print *, 'potency for strike-slip, dip-slip, tensile and explosive'
- ! read *, pot1, pot2, pot3, pot4 
+  real(kind=4), dimension(5) :: alpha, x, y, z, depth, dip, pot1, pot2, pot3, pot4 ! input values for dc3d0
+  real(kind=4), dimension(5) :: ux, uy, uz, uxx, uyx, uzx, uxy, uyy, uzy, uxz, uyz, uzz, iret ! values returned by dc3d0
+  integer :: out_unit = 1, in_unit = 2 ! opening i/o devices, please don't use 5 or 6 (special meaning)
+  integer :: stat ! this is for checking eof while reading file
+  integer :: j=1, i ! those are counters
 
   open(unit=in_unit, file='param.txt', action='read') 
-  read(in_unit, *) alpha, x, y, z, depth, dip, pot1, pot2, pot3, pot4
+  
+  !Reading file
+  do 
+    read(in_unit, *, iostat=stat) alpha(j), x(j), y(j), z(j), depth(j), dip(j), pot1(j), pot2(j), pot3(j), pot4(j)
+    !if(stat /= 0) exit   checking if eof
+    if(stat < 0) then
+      print *, 'Reached eof'
+      j = j-1
+      exit
+    else if(stat > 0) then
+      print *, 'Error in file format'
+      exit
+    end if
+    j = j+1
+  end do
+   
+  
+  !Calling subroutine and storing results in the i-th component of an array
+  do i=1, j
+    call dc3d0(alpha(i), x(i), y(i), z(i), depth(i), dip(i), pot1(i), pot2(i), pot3(i),&
+               & pot4(i), ux(i), uy(i), uz(i), uxx(i), uyx(i), uzx(i), uxy(i), uyy(i) &
+               &, uzy(i), uxz(i), uyz(i), uzz(i), iret(i))
+  end do
+ 
 
-  call dc3d0(alpha, x, y, z, depth, dip, pot1, pot2, pot3, pot4, ux, uy, uz, &
-             & uxx, uyx, uzx, uxy, uyy, uzy, uxz, uyz, uzz, iret)
+  open(unit=out_unit, file='results.txt', action='write', status='replace')
+ 
+  !Writing results on file 
+  do i = 1, j
+    write(out_unit, *) x(i), y(i), uz(i) 
 
-  open(unit=out_unit, file='hello.txt', action='write', status='replace')
-  write(out_unit, *) uxx, ' ', uyy, ' ', uzz
+  end do  
+
   close(out_unit)
   close(in_unit)
  
